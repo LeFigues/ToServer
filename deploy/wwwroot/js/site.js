92e0edafd41c9eb25e805e7ef39f73b2b9b47093
Miguel Figueroa <miguel.figueroa@meta-dao.com>
2026-02-27 19:11:04 -0400

    promocarnaval

---
+++ deploy/wwwroot/js/site.js
@@ -0,0 +1,72 @@
+﻿
+
+function printContent(id) {
+    var contentToPrint = document.getElementById("contentToPrint");
+    var printWindow = window.open('', '_blank');
+    printWindow.document.write('<html><head><title>#' + id + '</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Ft3lOC0hqib3bsL71LL6bA7p7TvMHik5bYRY5rGkBoq0CDQ+nOp6pNLHco2W0R7j" crossorigin="anonymous">');
+    printWindow.document.write('</head><body>');
+    printWindow.document.write(contentToPrint.innerHTML);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+}
+function printContentC(id) {
+    var contentToPrint = document.getElementById("contentToPrintC");
+    var printWindow = window.open('', '_blank');
+    printWindow.document.write('<html><head><title>#' + id + '</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Ft3lOC0hqib3bsL71LL6bA7p7TvMHik5bYRY5rGkBoq0CDQ+nOp6pNLHco2W0R7j" crossorigin="anonymous">');
+    printWindow.document.write('</head><body>');
+    printWindow.document.write(contentToPrint.innerHTML);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+}
+function printContentL(id) {
+    var contentToPrint = document.getElementById("contentToPrintL");
+    var printWindow = window.open('', '_blank');
+    printWindow.document.write('<html><head><title>#' + id + '</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Ft3lOC0hqib3bsL71LL6bA7p7TvMHik5bYRY5rGkBoq0CDQ+nOp6pNLHco2W0R7j" crossorigin="anonymous">');
+    printWindow.document.write('</head><body>');
+    printWindow.document.write(contentToPrint.innerHTML);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+}
+function printContentT(id) {
+    var contentToPrint = document.getElementById("contentToPrintT");
+    var printWindow = window.open('', '_blank');
+    printWindow.document.write('<html><head><title>#' + id + '</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Ft3lOC0hqib3bsL71LL6bA7p7TvMHik5bYRY5rGkBoq0CDQ+nOp6pNLHco2W0R7j" crossorigin="anonymous">');
+    printWindow.document.write('</head><body>');
+    printWindow.document.write(contentToPrint.innerHTML);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+}
+function printContentU(id) {
+    var contentToPrint = document.getElementById("contentToPrintU");
+    var printWindow = window.open('', '_blank');
+    printWindow.document.write('<html><head><title>#' + id + '</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Ft3lOC0hqib3bsL71LL6bA7p7TvMHik5bYRY5rGkBoq0CDQ+nOp6pNLHco2W0R7j" crossorigin="anonymous">');
+    printWindow.document.write('</head><body>');
+    printWindow.document.write(contentToPrint.innerHTML);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+}
+function printContentA4(id) {
+    var contentToPrintA4 = document.getElementById("contentToPrintA4");
+    var printWindow = window.open('', '_blank');
+    printWindow.document.write('<html><head><title>#' + id + '</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Ft3lOC0hqib3bsL71LL6bA7p7TvMHik5bYRY5rGkBoq0CDQ+nOp6pNLHco2W0R7j" crossorigin="anonymous">');
+    printWindow.document.write('</head><body>');
+    printWindow.document.write(contentToPrintA4.innerHTML);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+}
+function printContentLetter(id) {
+    var contentToPrintA4 = document.getElementById("contentToPrintLetter");
+    var printWindow = window.open('', '_blank');
+    printWindow.document.write('<html><head><title>#' + id + '</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Ft3lOC0hqib3bsL71LL6bA7p7TvMHik5bYRY5rGkBoq0CDQ+nOp6pNLHco2W0R7j" crossorigin="anonymous">');
+    printWindow.document.write('</head><body>');
+    printWindow.document.write(contentToPrintA4.innerHTML);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+}
